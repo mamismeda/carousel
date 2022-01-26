cc56b84b69b3d24cc3f70dc48d49a385d32fad18
mamismeda <amamismeda@gmail.com>
2022-01-26 22:00:30 +0400

    end of visual side of carousel and buttons funcitoanlity

---
+++ myscript.js
@@ -0,0 +1,36 @@
+let nextButton = document.querySelector(".butty.next");
+let prevButton = document.querySelector(".butty.prev");
+let slideItem = document.querySelectorAll(".item");
+
+nextButton.addEventListener("click", function(){
+    let activeItem = document.querySelector(".item.active");
+    activeItem.classList.remove('active');
+    activeItem.classList.add('slow');
+
+    if (activeItem.nextElementSibling != null) {
+        activeItem.nextElementSibling.classList.add('active');
+    }else{
+        slideItem[0].classList.add('active');
+    }
+
+   setTimeout(()=>{
+     document.querySelector('.item.slow').classList.remove('slow');
+   }, 700)
+})
+
+prevButton.addEventListener("click", function(){
+    let activeItem = document.querySelector(".item.active");
+    activeItem.classList.remove("active");
+    activeItem.classList.add('preslow');
+
+    if (activeItem.previousElementSibling !=null) {
+        activeItem.previousElementSibling.classList.add("active");
+    }else{
+        slideItem[slideItem.length - 1].classList.add("active");
+    }
+
+    setTimeout(()=>{
+        document.querySelector('.item.preslow').classList.remove('preslow');
+      }, 700)
+
+})
